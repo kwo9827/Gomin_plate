8c39e27279c8a36f6e55f6c1d56f635e6de06762
이은지 <dmswldk28@gmail.com>
2025-01-30 02:46:25 +0900

    실험중

---
+++ backend/sushi/src/main/java/com/ssafy/sushi/domain/notification/NotificationController.java
@@ -0,0 +1,15 @@
+package com.ssafy.sushi.domain.notification;
+
+import com.ssafy.sushi.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+}
