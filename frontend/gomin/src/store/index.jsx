0dc9797f94abba120ad0c0af5500452722f1a6f7
LSH <121619411+Choguesung@users.noreply.github.com>
2025-01-30 23:06:58 +0900

    refactor : add memberReducer

--- frontend/gomin/src/store/index.jsx
+++ frontend/gomin/src/store/index.jsx
@@ -1,16 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import sushiReducer from './slices/sushiSlice';
-import answerReducer from './slices/answerSlice';
-import notificationReducer from './slices/notificationSlice';
-
-import currentMember from './slices/memberSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import sushiReducer from "./slices/sushiSlice";
+import answerReducer from "./slices/answerSlice";
+import notificationReducer from "./slices/notificationSlice";
+import memberReducer from "./slices/memberSlice";
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        sushi: sushiReducer,
-        answer: answerReducer,
-        notification: notificationReducer,
-    },
+  reducer: {
+    auth: authReducer,
+    sushi: sushiReducer,
+    answer: answerReducer,
+    notification: notificationReducer,
+    member: memberReducer,
+  },
 });
