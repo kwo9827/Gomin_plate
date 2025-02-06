2bcbc3b9834768f2a15e776896f2df9f64cdd487
이상호 <tkdgh6427@naver.com>
2025-02-06 10:56:39 +0900

    test : login

--- frontend/gomin/src/api/axios.jsx
+++ frontend/gomin/src/api/axios.jsx
@@ -6,20 +6,22 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-
-    // const token = useSelector((state) => state.member?.accessToken || "test");
+    console.log("들어온거확인인");
+    const token = useSelector((state) => state.member?.accessToken || "test");
 
     // if (token) {
     //     config.headers.Authorization = `Bearer ${token}`;
     // } else {
     //     config.headers.Authorization = `Bearer test`;
     // }
-
+    console.log("동작확인");
     console.log("axios 파일에서의 토큰 찍기 : ", token);
 
-    // config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
+    console.log("헤더1 : ", config.headers.Authorization);
 
     config.headers.Authorization = `Bearer test`;
+    console.log("헤더2 : ", config.headers.Authorization);
 
     return config;
 });
