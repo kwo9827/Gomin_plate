e410ca8e0c1a80b89226b26305ca40c178414155
이상호 <tkdgh6427@naver.com>
2025-02-06 11:14:37 +0900

    test : login

--- frontend/gomin/src/api/axios.jsx
+++ frontend/gomin/src/api/axios.jsx
@@ -1,20 +1,16 @@
-import axios from "axios";
-import { store } from "../store"; // store import 경로 확인 필요
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
-    const token = store.getState().member?.accessToken; // Redux store에서 직접 가져오기
+    const token = useSelector((state) => state.member.accessToken);
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+    config.headers.Authorization = `Bearer ${token}`;
 
     return config;
-}, (error) => {
-    return Promise.reject(error);
 });
 
 export default api;
