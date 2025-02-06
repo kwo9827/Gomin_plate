c187932e724cd89709d6ef5a473d9f9bb5821dec
이상호 <tkdgh6427@naver.com>
2025-02-06 11:50:50 +0900

    test : login

--- frontend/gomin/src/api/axios.jsx
+++ frontend/gomin/src/api/axios.jsx
@@ -1,14 +1,15 @@
 import axios from 'axios';
-import { useSelector } from 'react-redux';
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
-    const token = useSelector((state) => state.member.accessToken);
+    // const token = useSelector((state) => state.member.accessToken);
 
-    config.headers.Authorization = `Bearer ${token}`;
+    // config.headers.Authorization = `Bearer ${token}`;
+
+    config.headers.Authorization = `Bearer test`;
 
     return config;
 });
