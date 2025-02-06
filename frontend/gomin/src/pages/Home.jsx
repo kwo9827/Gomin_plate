38dee364c472d6d1aad23ffadb29c556422070a9
이상호 <tkdgh6427@naver.com>
2025-02-06 17:45:17 +0900

    test : nicknameSet

--- frontend/gomin/src/pages/Home.jsx
+++ frontend/gomin/src/pages/Home.jsx
@@ -63,22 +63,13 @@ const Home = () => {
 
   /** 신규 유저는 닉네임 세팅을 해야함 */
   const isNew = useSelector((state) => state.member?.isNew);
-  const userNickName = localStorage.getItem("userNickName");
 
   useEffect(() => {
     /** 새로운 유저거나, 닉네임이 없으면 모달 오픈 */
-    if (isNew || userNickName) {
+    if (isNew) {
       openModal();
     }
-  }, [isNew, userNickName]);
-
-  // 닉네임이 설정되면 모달을 닫기
-  useEffect(() => {
-    if (userNickName) {
-      closeModal(); // 닉네임이 있으면 모달을 닫음
-    }
-  }, [userNickName]);
-  /** 여기 까지 */
+  }, [isNew]);
 
   return (
     <>
