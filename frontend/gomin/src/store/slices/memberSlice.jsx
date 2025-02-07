9da23905027a39abe7619be51e6688181ed258ba
이상호 <tkdgh6427@naver.com>
2025-02-07 15:24:42 +0900

    refactor : memberSlice

--- frontend/gomin/src/store/slices/memberSlice.jsx
+++ frontend/gomin/src/store/slices/memberSlice.jsx
@@ -4,7 +4,7 @@ const initialState = {
   nickname: localStorage.getItem("userNickname") || "",
   email: "",
   likesReceived: 0,
-  accessToken: "1",
+  accessToken: "",
   refreshToken: "",
   isNew: false,
 };
