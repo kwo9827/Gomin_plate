7b23ca6b8562f62badba85bbfba7207d8c1f9932
이상호 <tkdgh6427@naver.com>
2025-02-07 15:21:53 +0900

    fix : useParams library add

--- frontend/gomin/src/pages/SushiAnswerDetail.jsx
+++ frontend/gomin/src/pages/SushiAnswerDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAnswerDetail } from "../store/slices/answerSlice";
 import PostItModal from "../components/PostItModal";
