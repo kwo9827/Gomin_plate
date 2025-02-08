6c98ee5932f234a89b3599b6ff809ddb0f248e9b
2m7r <nubddagi@naver.com>
2025-02-08 15:01:33 +0900

    feat:네비게이션바디자인및버튼동작[S12P11B109-172]

--- frontend/gomin/src/pages/Home.jsx
+++ frontend/gomin/src/pages/Home.jsx
@@ -48,14 +48,14 @@ const Home = () => {
   }, [dispatch]);
 
   /** 로그인 상태가 아니면 인트로 페이지로 리다이렉트 */
-  // const navigate = useNavigate();
-  // const accessToken = useSelector((state) => state.member?.accessToken);
+  const navigate = useNavigate();
+  const accessToken = useSelector((state) => state.member?.accessToken);
 
-  // useEffect(() => {
-  //   if (!accessToken) {
-  //     navigate("/", { replace: true });
-  //   }
-  // }, [accessToken, navigate]);
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/", { replace: true });
+    }
+  }, [accessToken, navigate]);
   /** 여기 까지 */
 
   // /** 신규 유저는 닉네임 세팅을 해야함 */
