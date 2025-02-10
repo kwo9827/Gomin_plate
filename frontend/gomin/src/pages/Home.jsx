d8c672131571aec3b60ca8579f16e4c5801e2c65
이상호 <tkdgh6427@naver.com>
2025-02-10 10:26:17 +0900

    refactor : login redirect

--- frontend/gomin/src/pages/Home.jsx
+++ frontend/gomin/src/pages/Home.jsx
@@ -47,17 +47,6 @@ const Home = () => {
     dispatch(fetchUnreadExists());
   }, [dispatch]);
 
-  /** 로그인 상태가 아니면 인트로 페이지로 리다이렉트 */
-  const navigate = useNavigate();
-  const accessToken = useSelector((state) => state.member?.accessToken);
-
-  useEffect(() => {
-    if (!accessToken) {
-      navigate("/", { replace: true });
-    }
-  }, [accessToken, navigate]);
-  /** 여기 까지 */
-
   return (
     <>
       {/* 배경 이미지 */}
