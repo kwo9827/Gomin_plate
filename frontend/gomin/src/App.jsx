ff436e476fb39c0c081c618d67ba97251134a2bc
심규빈 <skb0516@naver.com>
2025-02-12 15:09:13 +0900

    fix: Protected Router로 home 접근

--- frontend/gomin/src/App.jsx
+++ frontend/gomin/src/App.jsx
@@ -11,6 +11,7 @@ import PostSushi from "./pages/PostSushi";
 import Navbar from "./components/NavBar";
 import OAuthCallback from "./components/OAuthCallback";
 import ErrorPage from "./pages/ErrorPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const location = useLocation();
@@ -40,7 +41,17 @@ function App() {
       {shouldShowNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Intro />} />
-        <Route path="/home" element={<Home />} />
+
+        {/* <Route path="/home" element={<Home />} /> */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="/mysushilist" element={<MySushiList />} />
         <Route path="/myanswerlist" element={<MyAnswerList />} />
         <Route path="/sushidetail/:sushiId" element={<SushiDetail />} />
