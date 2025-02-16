e8e6a38d63c849e6b501979dbaceccf8fd0c98c0
이은지 <dmswldk28@gmail.com>
2025-02-16 19:33:47 +0900

    공유하기 링크 타고 들어왔을 시에 이미 마감된 초밥은 볼 수 없음

--- frontend/gomin/src/pages/Home.jsx
+++ frontend/gomin/src/pages/Home.jsx
@@ -13,6 +13,7 @@ import SushiUnlock from "../components/SushiUnlock";
 import PostSushi from "./PostSushi";
 import SushiUnlockBar from "../components/SushiUnlockBar";
 import BgmContext from "../context/BgmProvider";
+import CommonAlertModal from "../components/CommonAlertModal";
 
 import { useSpring, animated } from "@react-spring/web";
 
@@ -96,6 +97,18 @@ const Home = () => {
     setShowAnswerSubmitModal(true);
   };
 
+  const [alertModal, setAlertModal] = useState({
+    isOpen: false,
+    message: "",
+  });
+
+  const showAlert = (message) => {
+    setAlertModal({
+      isOpen: true,
+      message,
+    });
+  };
+
   // useSSE();
   const isSSEConnected = useSSE();
 
@@ -134,6 +147,13 @@ const Home = () => {
   const handleImageLoad = (image) => {
     setImagesLoaded((prevState) => ({ ...prevState, [image]: true }));
   };
+
+  useEffect(() => {
+    if (isSushiViewOpen && selectedSushiData?.isClosed) {
+      showAlert("이미 마감된 초밥이에요!");
+      setIsSushiViewOpen(false); // 모달 열리지 않도록 막기
+    }
+  }, [isSushiViewOpen, selectedSushiData]);
 
   // 배경 이미지 로드 완료 처리
   useEffect(() => {
@@ -344,7 +364,7 @@ const Home = () => {
               </div>
             )} */}
             <audio ref={audioRef} src={plate} />
-            {selectedSushiData && (
+            {selectedSushiData && isSushiViewOpen && !selectedSushiData.isClosed && (
               <SushiView
                 isOpen={isSushiViewOpen}
                 onClose={() => setIsSushiViewOpen(false)}
@@ -369,6 +389,11 @@ const Home = () => {
             <NotificationModal
               isOpen={isNotificationOpen}
               onClose={closeNotification}
+            />
+            <CommonAlertModal
+              isOpen={alertModal.isOpen}
+              onClose={() => setAlertModal({ isOpen: false, message: "" })}
+              message={alertModal.message}
             />
           </div>
         </div>
