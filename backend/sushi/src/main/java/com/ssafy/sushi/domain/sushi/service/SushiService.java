cb59bfb6846a4725284f457fc93582b469d65e78
심규빈 <skb0516@naver.com>
2025-02-18 00:14:36 +0900

    refactor: sushiExposure 저장시 동시성 이슈 방지

--- backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/service/SushiService.java
+++ backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/service/SushiService.java
@@ -140,22 +140,14 @@ public class SushiService {
     @Transactional
     public void updateSushiExposure(Integer userId, Sushi sushi) {
         // 사용자와 초밥에 대한 exposure 조회
-        SuShiExposure exposure = sushiExposureRepository.findByUserIdAndSushiId(userId, sushi.getId());
-
-        // 이미 존재하면 timestamp만 갱신
-        if (exposure != null) {
-            exposure.updateTimestamp();
-        } else {
-            // 존재하지 않으면 새로운 exposure 생성
-            User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-
-            exposure = SuShiExposure.builder()
-                    .user(user)
-                    .sushi(sushi)
-                    .timestamp(now())
-                    .build();
-            sushiExposureRepository.save(exposure);
-        }
+//        saveOrUpdate 패턴
+        sushiExposureRepository.save(
+                SuShiExposure.builder()
+                        .user(userRepository.getReferenceById(userId))
+                        .sushi(sushi)
+                        .timestamp(now())
+                        .build()
+        );
     }
 
     private Sushi getSushiById(Integer sushiId) {
