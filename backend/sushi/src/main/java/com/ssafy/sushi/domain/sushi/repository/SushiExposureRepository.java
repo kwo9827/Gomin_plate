4cdf3a43fa0ee97c61d6b671620d61c9f813607a
심규빈 <skb0516@naver.com>
2025-02-18 01:09:46 +0900

    fix: @Modifying 누락 수정

--- backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/repository/SushiExposureRepository.java
+++ backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/repository/SushiExposureRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.sushi.domain.sushi.entity.SuShiExposure;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,6 +19,7 @@ public interface SushiExposureRepository extends JpaRepository<SuShiExposure, Lo
     @Query("SELECT se FROM SuShiExposure se WHERE se.user.id = :userId AND se.sushi.id = :sushiId")
     Optional<SuShiExposure> findByUserIdAndSushiIdWithLock(@Param("userId") Integer userId, @Param("sushiId") Integer sushiId);
 
+    @Modifying
     @Query(value = """
             INSERT INTO sushi_exposure (user_id, sushi_id, timestamp)
             VALUES (:userId, :sushiId, :timestamp)\s
