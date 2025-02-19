33a44b08a1c341478e3f1793d15c2dd0f50d8566
심규빈 <skb0516@naver.com>
2025-02-19 09:30:09 +0900

    refactor: 유통기한 24시간으로

--- backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/constants/SushiConstants.java
+++ backend/sushi/src/main/java/com/ssafy/sushi/domain/sushi/constants/SushiConstants.java
@@ -1,5 +1,5 @@
 package com.ssafy.sushi.domain.sushi.constants;
 
 public final class SushiConstants {
-    public static final int EXPIRATION_MINUTES = 60 * 3; // 분
+    public static final int EXPIRATION_MINUTES = 60 * 24; // 분
 }
