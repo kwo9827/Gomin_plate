9f9cc54332bbacdd0a5a96c5af3ad99ee1092641
심규빈 <skb0516@naver.com>
2025-02-19 17:59:21 +0900

    refactor: 엔티티에서 unique key 처리

--- backend/sushi/src/main/java/com/ssafy/sushi/global/infra/fcm/FcmToken.java
+++ backend/sushi/src/main/java/com/ssafy/sushi/global/infra/fcm/FcmToken.java
@@ -8,7 +8,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "fcm_tokens")
+@Table(name = "fcm_tokens",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "UK_fcm_tokens_user_token",
+                        columnNames = {"user_id", "token"}
+                )
+        })
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FcmToken {
