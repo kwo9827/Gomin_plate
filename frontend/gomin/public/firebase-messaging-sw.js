cc1c183e161560622463686d25e280a7bf961723
이은지 <dmswldk28@gmail.com>
2025-02-20 10:31:37 +0900

    ios 정말 짱난다다

--- frontend/gomin/public/firebase-messaging-sw.js
+++ frontend/gomin/public/firebase-messaging-sw.js
@@ -58,22 +58,3 @@ self.addEventListener("notificationclick", (event) => {
       })
   );
 });
-
-const CACHE_NAME = "pwa-cache-v10";
-const urlsToCache = ["/", "/index.html"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
